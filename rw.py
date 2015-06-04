3e7d551dc22055101cc1a228485a533db568a39d
Vitalii Demianiuk <chavit92@gmail.com>
2015-06-04 04:00:33 +0400

    =image_routine

--- rw.py
+++ rw.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-
 from PIL import Image, ImageDraw
 import scipy.spatial as ss
 import random
@@ -19,7 +18,8 @@ if (len(sys.argv) == 4):
     arg_S = int(sys.argv[1])
     alpha = float(sys.argv[2])
     sampleSize = int(sys.argv[3])	
-print("S = {:d} , alpha = {:.3f}, n = {:.3f}".format(arg_S, alpha, sampleSize))
+print("S = {:d}, alpha = {:.3f}, n = {:.3f}".format(arg_S, alpha, sampleSize))
+
 
 
 seed = 23;
@@ -47,28 +47,50 @@ def fromImageToArray(image):
 			p[0][a] = t % 100
 			p[1][a] = t // 100
 			a += 1
+	putImage(np.array(p), isNorm = False)
 	for i in range(sampleSize):
 		ind = random.randint(0, a - 1)
 		k[0][i] = p[0][ind]
 		k[1][i] = p[1][ind]
 	return np.array(k) - 50
 
-def showImage(new):
+
+picLen = 120
+bordLen = 20
+rowLen = 3;
+newIm = Image.new('P', (rowLen*picLen,rowLen*picLen), color = 255) 
+imagesAdded = 0
+
+def normalize(a):
+	a = a - a.min()
+	a = a / a.max()
+	a = a * (picLen - bordLen)
+	return a + bordLen // 2
+
+
+def putImage(new, isNorm = True):
+	global imagesAdded
 	newX = new.copy()
-	newX += 400
+	if isNorm:
+		newX[0] = normalize(newX[0]) 
+		newX[1] = normalize(newX[1])
 	newX = newX.astype(int)
-	size = (800, 800)
-	newIm = Image.new('P', size, color = 255) 
+	newX[0] += imagesAdded % rowLen * picLen
+	newX[1] += imagesAdded // rowLen * picLen
+	imagesAdded += 1
 	for i in range(len(newX[0])):
 		newIm.putpixel((newX[0][i],newX[1][i]), 0)
+	
+def showImage():
 	newIm.show()	
+	newIm.save("S={:d}_alpha={:.3f}_n={:.3f}".format(arg_S, alpha, sampleSize), 'bmp')
 
 
 def evalNearest(forTree):
 	tree = ss.cKDTree(forTree)
 	res = 0
 	for i in range(len(forTree)):
-		pp = tree.query(forTree[i],arg_S+1,p=2)[0] #find nearest arg_S distances
+		pp = tree.query(forTree[i],arg_S,p=2)[0] #find nearest arg_S+1 distances
 		pp = np.power(pp,(1-alpha)*2)
 		res += np.sum(pp)
 	return res
@@ -95,6 +117,7 @@ imX = fromImageToArray('x.bmp')
 imY = fromImageToArray('y.bmp')
 imZ = fromImageToArray('z.bmp')
 
+
 S = np.concatenate((imX, imY, imZ), axis = 0)
 
 mat = np.zeros((6,6))
@@ -104,21 +127,17 @@ for i in range(6):
 
 S = mat.dot(S)
 Y = np.vsplit(S, np.array([2, 4]))
-showImage(Y[0])
-showImage(Y[1])
-showImage(Y[2])
+putImage(Y[0])
+putImage(Y[1])
+putImage(Y[2])
 ica = FastICA(n_components = 6, max_iter=2000, random_state = seed)
 ans = ica.fit_transform(np.transpose(S))
 
 
 S = np.transpose(ans)
-S = S * 1000
-
 
 
 best = 1000000000000
-
-
 for i in range(1,6):
 	S[i], S[1] = S[1].copy(), S[i].copy()
 	for j in range(3,6):
@@ -131,10 +150,11 @@ for i in range(1,6):
 		S[3], S[j] = S[j].copy(), S[3].copy()
 	S[1], S[i] = S[i].copy(), S[1].copy()
  
+
 Y = np.vsplit(l, np.array([2, 4]))
-showImage(Y[0])
-showImage(Y[1])
-showImage(Y[2])
+putImage(Y[0])
+putImage(Y[1])
+putImage(Y[2])
 print("Best = ",best)
 print("Maximum sum of mutual infromation = ", -best)
-
+showImage()
